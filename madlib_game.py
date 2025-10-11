beadc433b567c578377a464d1a360d903cffd3cb
millani05 <millaniyasirahkhabir@gmail.com>
2025-10-11 12:08:44 -0400

    Add files via upload

---
+++ madlib_game.py
@@ -0,0 +1,30 @@
+
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+adj5 = input("Adjective: ")
+adj6 = input("Adjective: ")
+adj7 = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+verb4 = input("Verb: ")
+verb5 = input("Verb: ")
+
+
+
+
+madlib = f"""Once upon a time, in a {adj1} kingdom, there lived a {adj2} girl who had no idea she was born with {noun1} powers.
+One day, a {adj3} prince tried to {verb1} her awake with a true love’s kiss.
+The moment their lips met, a {adj4} burst of magic shot out and made the sky {verb2} with {noun2}!
+The girl opened her eyes, glowing like a {noun3}, and realized she could now {verb3} anything she wished.
+She used her {adj5} sorcery to help the prince become the most {adj6} man in the land.
+Together they {verb4} across the kingdom, spreading {noun4} wherever they went.
+And every time they kissed, her powers grew even more {adj7}, proving that true love can {verb5} the impossible."""
+
+print(madlib)
